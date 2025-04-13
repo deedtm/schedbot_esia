2bc5be8f03a029626e236f2947f6d30dd9140637
deedtm <misha.nikto000@gmail.com>
2025-04-13 23:13:31 +0400

    + argument for logging level

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ from config.telegram import TOKEN
 from telegram.schedbot import Schedbot
 
 from log.utils import disable_loggers
+import argparse
 
 async def main():
     schedbot = Schedbot(TOKEN)
     await schedbot.start()
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+if __name__ == "__main__":  
+    parser = argparse.ArgumentParser(description='Schedbot ESIA')
+    parser.add_argument('--log-level', 
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], 
+                        default='INFO',
+                        help='Set the logging level')
+    args = parser.parse_args()
+    logging.basicConfig(level=getattr(logging, args.log_level))
     disable_loggers(('websockets', 'uc.connection', 'nodriver.core'))
     asyncio.run(main())
